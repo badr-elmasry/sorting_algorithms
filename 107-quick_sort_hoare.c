5d1dbcad85bdbbe68623844b44de919142954c6c
badr-elmasry <badr.ck.elmasry@gmail.com>
2024-02-20 16:48:05 -0800

    push

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -1,62 +1,70 @@
 #include "sort.h"
 /**
-  * quick_sort_hoare - quicksort algorithm
-  * @array: array to be sorted
-  * @size: size of array
-  */
+* partition - quicksort using Hoare version
+* @array: array to sort
+* @min: lowest index
+* @max: highest index
+* @size: size of the array
+*
+* Return: partition index
+*/
+size_t partition(int *array, ssize_t min, ssize_t max, size_t size)
+{
+	int swap, pivot;
+
+	pivot = array[max];
+	while (min <= max)
+	{
+		while (array[min] < pivot)
+			min++;
+		while (array[max] > pivot)
+			max--;
+		if (min <= max)
+		{
+			if (min != max)
+			{
+				swap = array[min];
+				array[min] = array[max];
+				array[max] = swap;
+				print_array(array, size);
+			}
+			min++;
+			max--;
+		}
+	}
+	return (max);
+}
+
+/**
+* quicksort - sorts a partition of an array of integers
+* @array: array to sort
+* @min: lowest index of the partition to sort
+* @max: highest index of the partition to sort
+* @size: size of the array
+*
+* Return: void
+*/
+void quicksort(int *array, ssize_t min, ssize_t max, size_t size)
+{
+	ssize_t pivot;
+
+	if (min < max)
+	{
+		pivot = partition(array, min, max, size);
+		quicksort(array, min, pivot, size);
+		quicksort(array, pivot + 1, max, size);
+
+	}
+}
+
+/**
+* quick_sort_hoare - sorts an array of integer with quick sort
+* @array: The array to sort
+* @size: The size of the array
+*/
 void quick_sort_hoare(int *array, size_t size)
 {
-	sort_alg(array, 0, size - 1, size);
-}
-
-/**
-  * sort_alg - sorting algorithm
-  * @arr: array
-  * @left: leftmost index
-  * @right: rightmost index
-  * @size: size of full array
-  */
-void sort_alg(int *arr, int left, int right, size_t size)
-{
-	int pivot;
-
-	if ((right - left) < 2)
+	if (array == NULL || size < 2)
 		return;
-	pivot = split(arr, left, right, size);
-	sort_alg(arr, left, pivot, size);
-	sort_alg(arr, pivot, right, size);
-}
-
-/**
-  * split - pivot and split
-  * @arr: array
-  * @left: leftmost index
-  * @right:rightmost index
-  * @size: size of full index
-  * Return: pivot index
-  */
-int split(int *arr, int left, int right, size_t size)
-{
-	int i, i2, pivot, tmp;
-
-	pivot = arr[right];
-	i = left;
-	i2 = right;
-
-	while (1)
-	{
-		do i++;
-		while (arr[i] < pivot);
-		do i2--;
-		while (arr[i2] > pivot);
-		if (i < i2)
-		{
-			tmp = arr[i2];
-			arr[i2] = arr[i];
-			arr[i] = tmp;
-			print_array(arr, size);
-		}
-		else
-			return (i2);
-	}
+	quicksort(array, 0, size - 1, size);
 }
